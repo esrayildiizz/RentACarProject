7bba7974580fa53c824bb0818b35908427a2640e
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-01 20:33:25 +0300

    Yapici metot eklendi.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -10,7 +10,7 @@ namespace Business.Concrete
     public class CarManager : ICarService
     {
         ICarDal _carDal;
-        public CarManager(ICarDal carDal)
+        public CarManager(ICarDal carDal) //Yapıcı metot eklendi.
         {
             _carDal = carDal;
         }
