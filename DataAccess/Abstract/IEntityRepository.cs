3279f1069d262c4d133d4241a86bb07dfdb15175
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-14 17:03:49 +0300

    IEntityRepository alt yapisi olusturuldu.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T:class ,IEntity, new()
+    {
+        //Bütün ürünleri çek getir.
+        //Şartlı listeleme
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);  //tek bir data getirmek için
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
