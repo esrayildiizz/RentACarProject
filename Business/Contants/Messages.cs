2b2a164d9c80868d7f06de399e5836447e36e180
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-19 22:06:08 +0300

    Contants(sabitler) dosyasi olusturuldu.

---
+++ Business/Contants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Contants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Araba eklendi.";
+        public static string CarNameInvalid = "Araba ismi geçersiz..";
+        internal static string MaintenanceTime = "Sistem bakımda";
+        internal static string CarsListed = "Arabalar listelendi.";
+    }
+}
