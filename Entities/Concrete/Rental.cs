9c1d762405c8b3c08f958e744908ad6aa8245cc0
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-21 18:25:50 +0300

    Yeni entity'ler eklendi.

---
+++ Entities/Concrete/Rental.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Rental
+    {
+        public int RentalId { get; set; }
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+        public string RentDate { get; set; }
+        public string ReturnDate { get; set; }
+    }
+}
---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
