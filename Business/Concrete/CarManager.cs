d246b7c8b469f767f3321a69a73ce24e00171ecb
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-21 18:10:06 +0300

    ICarService 'a gore duzenleme yapildi.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Contants;
+using Core.Unitilies.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -16,29 +18,39 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
-        public IEnumerable<Car> GetAllByColorId()
+        public IDataResult<List<Car>> GetAllByColorId()
         {
             throw new NotImplementedException();
         }
 
-        public List<Car> GetAll()
+        public IDataResult<List<Car>> GetAll()
         {
-            return _carDal.GetAll();
+            if (DateTime.Now.Hour == 20)
+            {                                            //message
+                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime); //bakım zamanı demek.
+            }
+            // T data,message
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarsListed);
         }
 
-        public List<Car> GetAllByBrandId(int id)
+        public IDataResult<List<Car>> GetAllByBrandId(int id)
         {
-            return _carDal.GetAll(x => x.BrandId == id);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(x => x.BrandId == id));
         }
 
-        public List<Car> GetAllByColorId(int id)
+        public IDataResult<List<Car>> GetAllByColorId(int id)
         {
-            return _carDal.GetAll(x => x.ColorId == id);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(x => x.ColorId == id));
         }
 
-        public List<CarDetailsDto> GetCarDetails()
+        public IDataResult<List<CarDetailsDto>> GetCarDetails()
         {
-            return _carDal.GetCarDetails();
+            return new SuccessDataResult<List<CarDetailsDto>>(_carDal.GetCarDetails());
+        }
+
+        public IResult Add(Car car)
+        {
+            return new SuccessResult(Messages.CarAdded);
         }
     }
 }
